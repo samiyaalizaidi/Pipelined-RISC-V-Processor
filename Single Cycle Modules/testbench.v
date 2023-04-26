bda5f9fadae179b3d6f3d55cbc8a4a23e63a31aa
Samiya Ali Zaidi <95542517+samiyaalizaidi@users.noreply.github.com>
2023-04-26 23:19:28 +0500

    Update testbench.v

--- Single Cycle Modules/testbench.v
+++ Single Cycle Modules/testbench.v
@@ -6,18 +6,12 @@ module testbench();
     wire [4:0] rs2; wire [4:0] rd; wire [63:0] WriteData; wire [63:0] ReadData1;
     wire [63:0] ReadData2; wire [63:0] imm_data; 
     wire [63:0] mux1Out; wire [63:0] Result; wire [63:0]  Read_Data_Memory; wire [6:0] opcode; 
-    wire [1:0]ALUOp;
-    wire [2:0] funct3;
     
-    RISC_V_Processor p(clk, reset, PC_Out, instruction, rs1, rs2, rd, WriteData, ReadData1, ReadData2, imm_data, mux1Out, Result, Read_Data_Memory, opcode, ALUOp, funct3);
+    RISC_V_Processor p(clk, reset, PC_Out, instruction, rs1, rs2, rd, WriteData, ReadData1, ReadData2, imm_data, mux1Out, Result, Read_Data_Memory, opcode);
     
     initial begin
         clk = 0;
         reset = 0;
-//        #35 reset = 1;
-//        #10 reset = 0;
-//        #35 reset = 1;
-//        #10 reset = 0;
     end
     
     always 
