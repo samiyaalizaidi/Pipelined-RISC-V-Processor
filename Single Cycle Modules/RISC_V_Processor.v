7a8d55468b1275e771cb3d9fda3761a184b2b243
Samiya Ali Zaidi <95542517+samiyaalizaidi@users.noreply.github.com>
2023-04-26 23:18:34 +0500

    Update RISC_V_Processor.v

--- Single Cycle Modules/RISC_V_Processor.v
+++ Single Cycle Modules/RISC_V_Processor.v
@@ -5,25 +5,24 @@ module RISC_V_Processor(
     output [63:0] PC_Out, output [31:0] instruction, output [4:0] rs1,
     output [4:0] rs2, output [4:0] rd, output [63:0] Write_Data, output [63:0] ReadData1, 
     output [63:0] ReadData2, output [63:0] imm_data, 
-    output [63:0] mux1Out, output [63:0] Result, output [63:0] Read_Data_Memory, output [6:0] opcode, output [1:0]ALUOp,
-    output [2:0] funct3
+    output [63:0] mux1Out, output [63:0] Result, output [63:0] Read_Data_Memory, output [6:0] opcode
     );
     
     wire [63:0] PC_In0; 
-    wire [63:0]PC_In1;
-//    wire [2:0] funct3;
+    wire [63:0] PC_In1;
+    wire [2:0] funct3;
     wire [6:0] funct7;
 
     
     // for control signals
-	wire Branch; wire MemRead; wire MemtoReg; wire MemWrite; wire ALUSrc; 
-	wire RegWrite; 
-//	wire [1:0] ALUOp;
-	
-	// for the register file
-	wire [63:0] ReadData1; wire [63:0] ReadData2;
+    wire Branch; wire MemRead; wire MemtoReg; wire MemWrite; wire ALUSrc; 
+    wire RegWrite; 
+    wire [1:0] ALUOp;
+
+    // for the register file
+    wire [63:0] ReadData1; wire [63:0] ReadData2;
     wire [63:0] PC_In;
-	wire [3:0] Operation;
+    wire [3:0] Operation;
 		
     Program_Counter PC(clk, reset, PC_In, PC_Out);
     Adder add4(PC_Out, 64'd4, PC_In0);
