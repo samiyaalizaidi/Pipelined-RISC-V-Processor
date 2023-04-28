a845ab66aad2e6c63100a6299eef1625ea6a57e2
Samiya Ali Zaidi <95542517+samiyaalizaidi@users.noreply.github.com>
2023-04-28 17:08:04 +0500

    Create Mux.v

---
+++ Pipelined Modules/Mux.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module Mux
+(
+	input [63:0] a,
+	input [63:0] b,
+	input  sel,
+	
+	output reg [63:0]data_out
+);
+
+    always @ (*)
+    begin
+        if (!sel)
+            data_out = a;
+        else
+            data_out = b;
+    end
+
+endmodule
