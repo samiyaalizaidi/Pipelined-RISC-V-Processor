91da52ae705b141a821cb68976a037e014a8ec76
Samiya Ali Zaidi <95542517+samiyaalizaidi@users.noreply.github.com>
2023-04-28 17:15:55 +0500

    Update EX_MEM_Reg.v

--- Pipelined Modules/EX_MEM_Reg.v
+++ Pipelined Modules/EX_MEM_Reg.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
 module EX_MEM_Reg(
+    input flush,
     input clk,
     input [63:0] PC,
     input RegWrite, MemWrite, Branch, ZERO, MemtoReg, MemRead,
@@ -15,18 +16,33 @@ module EX_MEM_Reg(
     output reg [4:0] RD_S
 );
     
-    always @(negedge clk) begin
-        RegWrite_S = RegWrite;
-        MemtoReg_S = MemtoReg;
-        Branch_S = Branch;
-        ZERO_S = ZERO;
-        MemWrite_S = MemWrite;
-        MemRead_S = MemRead;
-        Branch_Address_S = Branch_Address;
-        Result_S = Result;
-        Write_Data_Mem_S = Write_Data_Mem;
-        Funct_S = Funct;
-        RD_S = RD;
+    always @(posedge clk) begin
+        if (flush) begin
+            RegWrite_S = 0;
+            MemtoReg_S = 0;
+            Branch_S = 0;
+            ZERO_S = 0;
+            MemWrite_S = 0;
+            MemRead_S = 0;
+            Branch_Address_S = 0;
+            Result_S = 0;
+            Write_Data_Mem_S = 0;
+            Funct_S = 0;
+            RD_S = 0;
+        end 
+        else begin 
+            RegWrite_S = RegWrite;
+            MemtoReg_S = MemtoReg;
+            Branch_S = Branch;
+            ZERO_S = ZERO;
+            MemWrite_S = MemWrite;
+            MemRead_S = MemRead;
+            Branch_Address_S = Branch_Address;
+            Result_S = Result;
+            Write_Data_Mem_S = Write_Data_Mem;
+            Funct_S = Funct;
+            RD_S = RD;
+        end
     end
     
 endmodule
