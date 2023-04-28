e547b2b41f77fee4cf94dd225968cac8ee91c228
Samiya Ali Zaidi <95542517+samiyaalizaidi@users.noreply.github.com>
2023-04-28 17:15:23 +0500

    Create ALU_64_bit.v

---
+++ Pipelined Modules/ALU_64_bit.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+module ALU_64_bit
+(
+	input [63:0]a, b,
+	input [3:0] ALUOp,
+	
+	output reg [63:0] Result,
+	output reg ZERO
+);
+
+wire BLT = 4'b1000; 
+
+always @ (*)
+begin
+	case (ALUOp)
+		4'b0000: Result = a & b; // AND
+		4'b0001: Result = a | b; // OR
+		4'b0010: Result = a + b; // ADD
+		4'b0110: Result = a - b; // SUB
+		4'b1100: Result = ~(a | b); // NOR
+		4'b1000: Result = a < b; // BLT
+		4'b1001: Result =  a + b; // ADDI
+		4'b1111: Result = a * (2'd2**b); // SLLI
+//		default: Result = 0;
+	endcase
+	
+	if (ALUOp == BLT & Result) ZERO = 1;
+	else ZERO = (Result == 0);
+end
+
+endmodule 
